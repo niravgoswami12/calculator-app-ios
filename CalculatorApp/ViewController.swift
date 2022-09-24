49d49cd96d0414703e8d2aba098487f3dabead92
Nirav Goswami <niravgoswami12@gmail.com>
2022-09-24 17:06:18 -0400

    header comment added

--- CalculatorApp/ViewController.swift
+++ CalculatorApp/ViewController.swift
@@ -1,8 +1,14 @@
 //
 //  ViewController.swift
 //  CalculatorApp
-//
+//  Description: Basic Calculator App
+//  Version: v1.0.0
 //  Created by Nirav Goswami on 2022-09-21.
+//
+//  Group #18
+//  Nirav Goswami (301252385)
+//  Samir Patel (301286671)
+//  Esha Naik (301297804)
 //
 
 import UIKit
